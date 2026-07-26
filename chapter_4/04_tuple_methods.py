c4fb804ae759d3bd6be1804bcca900ffceaf138c
Tanya Tiwari <tanyatiwari9505@gmail.com>
2026-07-26 15:54:49 +0530

    updated code

--- chapter_4/04_tuple_methods.py
+++ chapter_4/04_tuple_methods.py
@@ -1,1 +1,34 @@
-a=(3,45,67,456,87,False,"Rohan","Taehyung")
+a=(3,45,45,67,456,87,False,"Rohan","Taehyung")
+print(a)
+no=a.count(45)
+print(no)
+
+
+fruits = ("apple", "banana", "orange", "banana")
+print(fruits.index("banana"))
+
+
+
+fruits = ("apple", "banana", "orange", "banana")
+print(fruits.index("banana",3))
+
+
+t = (4, 2, 8, 1)
+print(len(t))  
+    
+print(max(t))      
+print(min(t))  
+    
+print(sum(t))     
+ 
+print(sorted(t))   
+
+print(tuple(reversed(t))) 
+ 
+print(5 in t)
+print(4 in t)
+
+
+my_tuple = (4, 2, 8)
+a,b,c = my_tuple
+print(a,b,c)
---
+++ chapter_4/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+a=(3,45,67,456,87,False,"Rohan","Taehyung")
+print(a)
+
+no=a.count(45)
+print(no)
