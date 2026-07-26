d12915a78ade4dc9418d307e63e1342ee6810c79
Tanya Tiwari <tanyatiwari9505@gmail.com>
2026-07-26 14:17:07 +0530

    updated code

--- practice_set_3/problem4.py
+++ practice_set_3/problem4.py
@@ -1,3 +1,4 @@
-space="Harry is a good  boy and"
+space="Harry is a good   boy and"
 
-print(space.find("  "))
+print(space.replace("   "," "))
+print(space) #Strings are immutable which means that you cannot change them by running functions on them
---
+++ practice_set_3/problem5.py
@@ -0,0 +1,10 @@
+letter="Dear tanya,You are good!"
+print(letter)
+
+
+letter="Dear tanya,\nYou are good!"
+print(letter)
+
+
+letter="Dear tanya,\n\tYou are good!"
+print(letter)
