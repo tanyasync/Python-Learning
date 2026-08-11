0654a2109e31a064d7ce8d5ab812a1ec58040152
Tanya Tiwari <tanyatiwari9505@gmail.com>
2026-08-11 12:35:47 +0530

    updated code

---
+++ chapter_6/practice_set_06/problem5.py
@@ -0,0 +1,8 @@
+l=["Harry", "Rohan", "Shubham", "Divya"]
+
+name=input("Enter your name: ")
+
+if(name in l):
+    print("your name is in list")
+else:
+    print("your name is not in list")
---
+++ chapter_6/practice_set_06/problem7.py
@@ -0,0 +1,8 @@
+post=input("Enter the post: ")
+
+
+if("Tanya".lower() in post.lower()):
+    print("This post is talking about Tanya")
+    
+else:
+    print("This post is not talking about Tanya")
