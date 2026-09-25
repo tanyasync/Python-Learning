bd46c2fee6e3136a341e1ffa949528a3395ec073
Tanya Tiwari <tanyatiwari9505@gmail.com>
2026-09-25 23:45:03 +0530

    updated code

--- chapter_7/practice_set_07/problem9.py
+++ chapter_7/practice_set_07/problem9.py
@@ -1,5 +1,9 @@
 n=int(input("enter a number: "))
 for i in range(1,n+1):
-    print(" "*(n-i),end="")
-    print("*"*i, end="")
+    if(i==1 or i==n):
+        print("*"*n, end="")
+    else:
+        print("*",end="")
+        print(" "*(n-2), end="")
+        print("*", end="")
     print("")
