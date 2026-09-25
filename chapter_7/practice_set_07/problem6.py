d6954bdc5e70ee0fb70c59fa48b785cd17b133cf
Tanya Tiwari <tanyatiwari9505@gmail.com>
2026-09-25 23:37:43 +0530

    updated code

--- chapter_7/practice_set_07/problem6.py
+++ chapter_7/practice_set_07/problem6.py
@@ -1,6 +1,6 @@
-n=int(input("Enter a number: "  ))
-
 n=int(input("enter the number: "))
+product=1
 for i in range(1, n+1):
-    product=product*1
+    product=product*i
     
+print(f"factorial of {n} is {product}")
---
+++ chapter_7/practice_set_07/problem7.py
@@ -0,0 +1,12 @@
+'''
+for n=3
+  *
+ ***
+*****
+'''
+
+n=int(input("enter a value: "))
+for i in range(1,n+1):
+    print(" "*(n-i), end="")
+    print("*"*(2*i-1), end="")
+    print("")
